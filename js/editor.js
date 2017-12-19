278a70563eee1d2e48031af3441b7c413cd19fe0
Frederik De Bleser <frederik@debleser.be>
2017-12-19 15:58:06 +0100

    Better recursive example.

--- js/editor.js
+++ js/editor.js
@@ -20,7 +20,7 @@ const GALLERY = [
     '-L0iXXTIHpU3fd0hXiEo',
     '-L0ibWrnaptySlFrOniw',
     '-L0itqXixdWAyoBenEMk',
-    '-L0jDyf3m4YplgAenOKy'
+    '-L0jGl9IhooqRuTF9wxS'
 ];
 
 class SeedPicker extends Component {
