ce0215395b7e0539ad8022e9bddb3c4a7c493567
TanviKumar <tanvikumar777@gmail.com>
2018-02-16 22:03:01 +0530

    Add seed to URL
    	- Adds seed to URL
    	- Generates based on seed in URL

--- js/core.js
+++ js/core.js
@@ -76,6 +76,16 @@ RegExp.escape = function(s) {
     return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 String.prototype.toTitleCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
